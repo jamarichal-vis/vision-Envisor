81bb342e9ed30c1ea50df297fda427f4404224ed
Alejandro Marichal <Alejandro Marichal@DESKTOP-C34LA1U>
2020-10-29 15:02:32 +0100

    Implementado la toma de decisiones para asignar un formulario (Basler o Flir) en función de la cámara que se vaya a conectar.

--- Recording/PanelManager.cs
+++ Recording/PanelManager.cs
@@ -30,7 +30,9 @@ namespace Recording
         /// <summary>
         /// Este atributo almacena el panel principal que se quiere controlar.
         /// </summary>
-        FlowLayoutPanel pnlCams;
+        FlowLayoutPanel flowLayoutPanelCameras;
+
+        Dictionary<Id, Panel> pnlCameras;
 
         public PanelManager(ref MilApp milApp, ref MIL_INT devSysGigeVision, ref MIL_INT devSysUsb3Vision, int numCams, ref FlowLayoutPanel pnl)
         {
@@ -38,54 +40,75 @@ namespace Recording
             this.devSysGigeVision = devSysGigeVision;
             this.devSysUsb3Vision = devSysUsb3Vision;
 
-            pnlCams = pnl;
+            flowLayoutPanelCameras = pnl;
+
+            pnlCameras = new Dictionary<Id, Panel>();
 
             ShowCams();
         }
 
         public void ShowCams()
         {
+            Form displayCameraForm = null;
+
             MIL_INT NbcamerasInGigeVisionSystem = milApp.GetNCameraInSystem(devSysGigeVision);
             MIL_INT NbcamerasInUsb3Vision = milApp.GetNCameraInSystem(devSysUsb3Vision);
 
             for (MIL_INT devDig = MIL.M_DEV0; devDig < NbcamerasInGigeVisionSystem; devDig++)
             {
                 Id id = new Id(devSysGigeVision, devDig);
-                DisplayCameraFlirForm displayCameraFlirForm = new DisplayCameraFlirForm(ref milApp, id: id);
 
-                AddPanel(displayCameraFlirForm);
+                Dictionary<string, string> camInfo = milApp.CamInfo(id.DevNSys, id.DevNCam);
+
+                if(camInfo["Vendor"] == "Basler")
+                    displayCameraForm = new DisplayCameraBaslerForm(ref milApp, id: id);
+                else if(camInfo["Vendor"] == "FLIR" || camInfo["Vendor"].Contains("FLIR"))
+                    displayCameraForm = new DisplayCameraFlirForm(ref milApp, id: id);
+
+                AddPanel(id, displayCameraForm);
             }
-            
+
             for (MIL_INT devDig = MIL.M_DEV0; devDig < NbcamerasInUsb3Vision; devDig++)
             {
                 Id id = new Id(devSysUsb3Vision, devDig);
-                DisplayCameraBaslerForm displayCameraBaslerForm = new DisplayCameraBaslerForm(ref milApp, id: id);
+                displayCameraForm = new DisplayCameraBaslerForm(ref milApp, id: id);
 
-                AddPanel(displayCameraBaslerForm);
+                AddPanel(id, displayCameraForm);
             }
+        }
+
+        public void Remove()
+        {
 
         }
 
-        public void AddPanel(DisplayCameraFlirForm displayCameraFlirForm)
+        /// <summary>
+        /// Esta función añade un panel a <see cref="pnlCameras">pnlCameras</see>/>.
+        /// Además, se añade la identificación del panel a través de su <see cref="Id">Id</see>/>.
+        /// </summary>
+        /// <param name="id">Id de la cámara que se esta conectando al panel.</param>
+        /// <param name="pnl">Panel que se quiere añadir.</param>
+        private void AddPanelToDict(Id id, Panel pnl)
+        {
+            pnlCameras.Add(id, pnl);
+        }
+
+        /// <summary>
+        /// Este método añade un panel tanto a <see cref="flowLayoutPanelCameras">pnlCams</see>/> y a <see cref="pnlCameras">pnlCameras</see>/>.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="displayCameraFlirForm"></param>
+        public void AddPanel(Id id, Form displayCameraFlirForm)
         {
             Panel panel = new Panel();
             panel.Size = displayCameraFlirForm.Size;
 
-            AddFormInPanel(displayCameraFlirForm, panel);
-
-            pnlCams.Controls.Add(panel);
-            pnlCams.ResumeLayout(false);
-        }
-        
-        public void AddPanel(DisplayCameraBaslerForm displayCameraFlirForm)
-        {
-            Panel panel = new Panel();
-            panel.Size = displayCameraFlirForm.Size;
+            AddPanelToDict(id, panel);
 
             AddFormInPanel(displayCameraFlirForm, panel);
 
-            pnlCams.Controls.Add(panel);
-            pnlCams.ResumeLayout(false);
+            flowLayoutPanelCameras.Controls.Add(panel);
+            flowLayoutPanelCameras.ResumeLayout(false);
         }
 
         /// <summary>
